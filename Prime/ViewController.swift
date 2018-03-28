17dc242b749754a91a66b5fd0a2722b1cf1c1948
201631053 <hubot@githhub@com>
2018-03-28 16:18:52 +0900

    add delegate

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var lblPrime: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btCheck(_ sender: Any) {
+        var isPrime = true
+        let number = Int(txtField.text!)
+        if number == 1 {
+            isPrime = false       }
+        
+        if number != 1 && number != 2 {
+            for i in 2 ..< number! {
+                if number! % i == 0 {
+                    isPrime = false
+                }
+            }
+        }
+        
+        if isPrime == true {
+            lblPrime.text = "Prime number"
+        } else {
+            lblPrime.text = "Not Prime number"
+    }
+ 
+    
+      
+}
+    
+    @IBAction func btReset(_ sender: Any) {
+        txtField.text = ""
+        lblPrime.text = "Label"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtField.resignFirstResponder()
+        return true
+        // called when 'return' key pressed. return NO to ignore.
+}
 }
 
